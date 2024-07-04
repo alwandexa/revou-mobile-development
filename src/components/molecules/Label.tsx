41153a1378d820d7ba5b6875124b1a7cb53ca652
alwandexa <alwan.wirawan@djembatandua.com>
2024-07-04 14:37:45 +0700

    style(functions): Create Label

---
+++ src/components/molecules/Label.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {StyleSheet, View} from "react-native";
+
+import {COLORS} from "../../constants/colors";
+import Typography from "../atoms/Typography";
+
+interface LabelProps {
+  variant?: "primary" | "outline" | "tertiary";
+  color?: "purple" | "blue" | "green" | "red" | "neutral";
+  children: string;
+}
+
+const Label = ({
+  variant = "primary",
+  color = "purple",
+  children,
+}: LabelProps) => {
+  const getVariantStyles = () => {
+    const variantStyle = variantStyles[variant] || {};
+    const primaryColor = COLORS[`${color}600`];
+    const secondaryColor = COLORS[`${color}100`];
+
+    let selectedStyle = {};
+    let textStyle = {};
+
+    if (variant === "primary") {
+      selectedStyle = {
+        backgroundColor: primaryColor,
+      };
+      textStyle = {
+        color: COLORS.neutral100,
+      };
+    } else if (variant === "outline") {
+      selectedStyle = {
+        borderColor: primaryColor,
+      };
+      textStyle = {
+        color: primaryColor,
+      };
+    } else if (variant === "tertiary") {
+      textStyle = {
+        color: primaryColor,
+      };
+      if (color === "neutral") {
+        selectedStyle = {
+          backgroundColor: COLORS.neutral300,
+        };
+      } else {
+        selectedStyle = {
+          backgroundColor: secondaryColor,
+        };
+      }
+    }
+
+    const viewStyle = {...variantStyle, ...selectedStyle};
+
+    return {viewStyle, textStyle};
+  };
+
+  return (
+    <View style={[styles.container, getVariantStyles().viewStyle]}>
+      <Typography
+        type="heading"
+        size="xsmall"
+        style={getVariantStyles().textStyle}>
+        {children}
+      </Typography>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    alignSelf: "flex-start",
+  },
+});
+
+const variantStyles = StyleSheet.create({
+  primary: {},
+  outline: {
+    borderWidth: 1,
+  },
+  tertiary: {},
+});
+
+export default Label;
