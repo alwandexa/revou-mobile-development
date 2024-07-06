5a0d56141ba97e4b60b7eb9e9c8236506cbd1131
alwan <alwan.wirawan@djembatandua.com>
2024-07-06 11:51:40 +0700

    style(functions): Create Icon Atom

---
+++ src/components/atoms/icon/Icon.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {StyleSheet} from "react-native";
+
+import {COLORS} from "../../../constants/colors";
+
+type IconName = "eye" | "eye-slash" | "heart" | "person";
+
+const iconMap: Record<
+  IconName,
+  React.FC<{width?: number; height?: number; fill?: string}>
+> = {
+  eye: require("./Eye").default,
+  "eye-slash": require("./EyeSlash").default,
+  heart: require("./Heart").default,
+  person: require("./Person").default,
+};
+
+type IconProps = {
+  name: IconName;
+  fill?: string;
+  width?: number;
+  height?: number;
+};
+
+const Icon = ({
+  name,
+  fill = COLORS.neutral700,
+  width = 24,
+  height = 24,
+  ...props
+}: IconProps) => {
+  const IconComponent = iconMap[name];
+
+  return <IconComponent fill={fill} width={width} height={height} {...props} />;
+};
+
+export default Icon;
+
+const styles = StyleSheet.create({});
