980ee3cbeba68998674b9dfd46cdb82e4bf19001
alwan <alwan.wirawan@djembatandua.com>
2024-07-08 06:59:08 +0700

    feat(functions): Create Profil Screen

---
+++ src/screens/Profil.tsx
@@ -0,0 +1,29 @@
+import React, {FunctionComponent} from "react";
+import {Image, SafeAreaView, StyleSheet} from "react-native";
+
+import Typography from "../components/atoms/Typography";
+import {COLORS} from "../constants/colors";
+
+const Profil: FunctionComponent = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={require("../assets/images/invest-logo.png")} />
+      <Typography
+        type="heading"
+        size="xxlarge"
+        style={{color: COLORS.neutral700}}>
+        Coming soon
+      </Typography>
+    </SafeAreaView>
+  );
+};
+
+export default Profil;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
