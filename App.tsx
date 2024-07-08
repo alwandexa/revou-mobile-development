7f69698347df83d3d3b64ca6e822e9d7351a4b0a
alwan <alwan.wirawan@djembatandua.com>
2024-07-08 07:02:04 +0700

    feat(functions): Create HomeTab

--- App.tsx
+++ App.tsx
@@ -3,23 +3,43 @@ import React, {FunctionComponent} from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Home, {HomeHeader} from "./src/screens/Home";
 import Login, {LoginHeader} from "./src/screens/Login";
 import Onboarding from "./src/screens/Onboarding";
-import Home, {HomeHeader} from "./src/screens/Home";
+import Profil from "./src/screens/Profil";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeTab = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: true,
+          header: HomeHeader,
+        }}
+      />
+      <Tab.Screen
+        name="Profil"
+        component={Profil}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const App: FunctionComponent = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: true,
-            header: HomeHeader,
-          }}
+          name="Onboarding"
+          component={Onboarding}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Login"
@@ -30,9 +50,11 @@ const App: FunctionComponent = () => {
           }}
         />
         <Stack.Screen
-          name="Onboarding"
-          component={Onboarding}
-          options={{headerShown: false}}
+          name="HomeTab"
+          component={HomeTab}
+          options={{
+            headerShown: false,
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
