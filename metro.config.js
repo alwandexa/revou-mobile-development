e7b07fe5510e2e46a39c0eb01dcf87c7d7043d88
alwan <alwan.wirawan@djembatandua.com>
2024-08-07 20:30:50 +0700

    build(modules): Setup Obfuscation

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require("@react-native/metro-config");
 
 /**
  * Metro configuration
@@ -7,5 +7,35 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
+
+const jsoMetroPlugin = require("obfuscator-io-metro-plugin")(
+  {
+    compact: false,
+    sourceMap: false,
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 1,
+    numbersToExpressions: true,
+    simplify: true,
+    stringArrayShuffle: true,
+    splitStrings: true,
+    stringArrayThreshold: 1,
+  },
+  {
+    runInDev: false,
+    logObfuscatedFiles: true,
+  },
+);
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  ...jsoMetroPlugin,
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
