78ff1f4f5a3bf7c533f8f09877d0197326350e80
alwan <alwan.wirawan@djembatandua.com>
2024-08-08 07:08:01 +0700

    ref(functions): Extract Validation Functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import InvestlyServices from "@services/InvestlyServices";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import axios, {AxiosError} from "axios";
+import analytics from "@react-native-firebase/analytics";
 
 dayjs.extend(relativeTime);
 
@@ -126,4 +128,148 @@ export const getUserData = async () => {
     console.error("Error getting user data: ", error);
     return null;
   }
+};
+
+export const validateEmail = async (email: string) => {
+  let isValid = false;
+  let message = "";
+
+  email = email.trim().toLowerCase();
+
+  if (email.length > 254) {
+    message = "Email terlalu panjang (max 254 karakter)";
+    return {isValid, message};
+  }
+
+  const illegalChars = /[(),<>:;\[\]"]/;
+  if (illegalChars.test(email)) {
+    message = "Email mengandung karakter ilegal ((),<>:;[])";
+    return {isValid, message};
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    message = "Format email salah (nama@domain.com)";
+    return {isValid, message};
+  }
+
+  try {
+    const checkEmailRequest = {email};
+    const response = await InvestlyServices.checkEmail(checkEmailRequest);
+
+    if (response.data.status) {
+      isValid = true;
+    }
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const axiosError = err as AxiosError<any>;
+      message = axiosError.response?.data.messages || "Email sudah digunakan";
+      analytics().logEvent("failed_validate_register_email", {email});
+    } else {
+      console.error("Unexpected error:", err);
+      message = "Terjadi kesalahan yang tidak terduga";
+    }
+  }
+
+  return {isValid, message};
+};
+
+export const validatePassword = (password: string) => {
+  let isValid = false;
+  let message = "";
+
+  const MIN_LENGTH = 8;
+  const MAX_LENGTH = 64;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
+    password,
+  );
+
+  if (password.length < MIN_LENGTH) {
+    message = `Password minimal ${MIN_LENGTH} karakter`;
+    return {isValid, message};
+  }
+
+  if (password.length > MAX_LENGTH) {
+    message = `Password maksimal ${MAX_LENGTH} karakter`;
+    return {isValid, message};
+  }
+
+  if (!hasUpperCase || !hasLowerCase || !hasNumbers || !hasSpecialChar) {
+    message =
+      "Password harus mengandung huruf besar-kecil, angka, dan karakter khusus";
+    return {isValid, message};
+  }
+
+  isValid = true;
+  return {isValid, message};
+};
+
+export const validatePasswordConfirmation = (
+  password: string,
+  confirmation: string,
+) => {
+  let isValid = false;
+  let message = "";
+
+  if (password !== confirmation) {
+    message = "Konfirmasi password tidak sesuai";
+    return {isValid, message};
+  }
+
+  isValid = true;
+  return {isValid, message};
+};
+
+export const validateName = (name: string) => {
+  let isValid = false;
+  let message = "";
+
+  if (name.trim().length === 0) {
+    message = "Nama tidak boleh kosong";
+    return {isValid, message};
+  }
+
+  if (name.trim().length < 3) {
+    message = "Nama minimal 3 karakter";
+    return {isValid, message};
+  }
+
+  isValid = true;
+  return {isValid, message};
+};
+
+export const validateUsername = async (username: string) => {
+  let isValid = false;
+  let message = "";
+
+  username = username.trim();
+  if (username.length === 0) {
+    return {isValid, message};
+  }
+
+  try {
+    const checkUserRequest = {username};
+    const response = await InvestlyServices.checkUsername(checkUserRequest);
+
+    if (response.data.status) {
+      message = response.data.messages;
+      analytics().logEvent("failed_validate_register_username", {username});
+    } else {
+      isValid = true;
+    }
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const axiosError = err as AxiosError<any>;
+      message = axiosError.response?.data.messages || "Validasi error";
+      isValid = true;
+    } else {
+      console.error("Unexpected error:", err);
+      message = "Terjadi kesalahan yang tidak terduga";
+    }
+  }
+
+  return {isValid, message};
 };
